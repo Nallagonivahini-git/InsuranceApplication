62d602021aa5922fdc53da5b1fba073c92bab950
Nallagoni <vahini.nallagoni@acsicorp.com>
2024-01-30 12:42:15 +0530

    Initial commit

---
+++ src/main/java/com/Insurance/InuranceApplication/InuranceApplication.java
@@ -0,0 +1,13 @@
+package com.Insurance.InuranceApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InuranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Insurance/InuranceApplication/InuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Insurance.InuranceApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
